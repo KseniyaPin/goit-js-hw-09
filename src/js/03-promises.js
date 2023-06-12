dca75656502a2df9931e0e54231348023a84079d
KseniyaPin <kontr.mat@gmail.com>
2023-06-12 15:40:34 +0200

    hw-9-1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -24,7 +24,7 @@ function onFormSubmit(evt) {
     createPromise(position, delay);
     delay += step;
   }
-  // evt.target.reset();
+  evt.target.reset();
 }
 
 function createPromise(position, delay) {
